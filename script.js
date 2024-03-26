25745e947101e5505323ce874eb66cbfe9e213fb
eleteo <ele.teo@stu.ocsb.ca>
2024-03-26 09:13:58 -0400

    task

--- script.js
+++ script.js
@@ -11,4 +11,45 @@
 
 'use strict';
 
+class Circle {
+    name = "Circle";
+    radius = 1;
+    area() {
+        return Math.PI*this.radius**2
+    }
+}
 
+class Rectangle {
+    name = "Rectangle";
+    length = 1;
+    width = 1;
+    area() {
+        return this.length * this.width;
+    }
+    perimeter() {
+        return this.length*2 + this.width*2;
+    }
+    is_square() {
+        if (this.length == this.width) {
+            return true;
+        }
+        else return false;
+    }
+}
+
+class Vehicle {
+    type = "";
+    num_wheels;
+    num_doors;
+    num_seats;
+    fuel = "";
+    can_fly = false;
+}
+
+let rec = new Rectangle();
+rec.name = "r";
+rec.length = 4;
+rec.width = 4;
+console.log(rec.area());
+console.log(rec.perimeter());
+console.log(rec.is_square());
